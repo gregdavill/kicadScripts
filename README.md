dad6d26da6c12e745c42ef46d9d60c157c0636af
Gregory Davill <greg.davill@gmail.com>
2018-04-01 11:19:21 +0930

    First commit of files.

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # kicadScripts
 A collection of scripts to automate PCB rendering and exporting gerbers.
+
+### Work inspired form the folling projects:
+* [PcbDraw](https://github.com/yaqwsx/PcbDraw)
+* [Scott Bezek's SplitFlap](https://github.com/scottbez1/splitflap)
+
+I found PcbDraw after I started writting my own tool to do the same function. It's probably a better tool for rendering nice images of PCBs. 
+
+# Usage #
+Currently only a single script:
+
+	python plot_board.py <PathToYourAwesomeProject.kicad_pcb>
+
+Files are placed within a `plot` directory in the folder of the .kicad_pcb file
+
+# Features #
+
+* creates all the gerber/drill files required for fab, renames and zips them up.
+* creates an SVG of the top and bottom sides of the board with nice colours and renders these to PNG with inkscape CLI.
+
+# Examples #
+
+![alt-text](Example/bosonFrameGrabber-Front.png "bosonFrameGrabber Front")
+
+![alt-text](Example/bosonFrameGrabber-Back.png "bosonFrameGrabber Front")
+
+# Improvements
+
+- Draw drill hits properly.
+- Trim images around PCB.
+- Improve code
+- Plot front and back Images next to each other on the same image.
+- Remove hardcode of inkscape path.
---
+++ plot_board.py
@@ -0,0 +1,265 @@
+'''
+    A python script example to create various plot files from a board:
+    Fab files
+    Doc files
+    Gerber files
+
+    Important note:
+        this python script does not plot frame references.
+        the reason is it is not yet possible from a python script because plotting
+        plot frame references needs loading the corresponding page layout file
+        (.wks file) or the default template.
+
+        This info (the page layout template) is not stored in the board, and therefore
+        not available.
+
+        Do not try to change SetPlotFrameRef(False) to SetPlotFrameRef(true)
+        the result is the pcbnew lib will crash if you try to plot
+        the unknown frame references template.
+'''
+
+import sys
+import os
+import pcbnew
+import time
+
+import logging
+import zipfile
+import shutil
+import subprocess
+
+import pcb_util
+
+from svg_processor import SvgProcessor
+
+from pcbnew import *
+from datetime import datetime
+from shutil import copy
+
+
+filename=sys.argv[1]
+project_name = os.path.splitext(os.path.split(filename)[1])[0]
+project_path = os.path.abspath(os.path.split(filename)[0])
+
+output_directory = os.path.join(project_path,'plot')
+
+temp_dir = os.path.join(output_directory, 'temp')
+shutil.rmtree(temp_dir, ignore_errors=True)
+os.makedirs(temp_dir)
+
+
+today = datetime.now().strftime('%Y%m%d_%H%M%S')
+
+board = LoadBoard(filename)
+
+pctl = PLOT_CONTROLLER(board)
+
+popt = pctl.GetPlotOptions()
+
+popt.SetOutputDirectory(temp_dir)
+
+# Set some important plot options:
+popt.SetPlotFrameRef(False)
+popt.SetLineWidth(FromMM(0.35))
+
+popt.SetAutoScale(False)
+popt.SetScale(1)
+popt.SetMirror(False)
+popt.SetUseGerberAttributes(False)
+popt.SetExcludeEdgeLayer(True);
+popt.SetScale(1)
+popt.SetUseAuxOrigin(True)
+popt.SetNegative(False)
+popt.SetPlotReference(False)
+popt.SetPlotValue(False)
+popt.SetPlotInvisibleText(False)
+
+# This by gerbers only (also the name is truly horrid!)
+popt.SetSubtractMaskFromSilk(False) #remove solder mask from silk to be sure there is no silk on pads
+
+
+# Visual Plot 
+def renderPNG(plot_plan, output_filename):
+    processed_svg_files = []
+    popt.SetDrillMarksType(PCB_PLOT_PARAMS.FULL_DRILL_SHAPE)
+
+    for layer_info in plot_plan:
+        if layer_info[4] == "Invert":
+            popt.SetNegative(True)
+        else:
+            popt.SetNegative(False)
+
+        pctl.SetLayer(layer_info[1])
+        pctl.OpenPlotfile(layer_info[0], PLOT_FORMAT_SVG, layer_info[2])
+        print layer_info[0]
+        pctl.PlotLayer()
+        time.sleep(0.01)
+        pctl.ClosePlot()
+        processor = SvgProcessor(pctl.GetPlotFileName())
+        def colorize(original):
+            # For invert to work we need to invert default colours. 
+            if layer_info[4] == "Invert":
+                if original.lower() == '#000000':
+                    return '#ffffff'
+                return '#000000'
+            else:
+                if original.lower() == '#000000':
+                    return layer_info[5]
+                return original
+        processor.apply_color_transform(colorize)
+        if layer_info[4] == "Invert":
+            # Invert call will group all svg objects into a Mask then create a Blank Rectangel to mask out off.
+            processor.Invert(layer_info[5], layer_info[6])
+        else:
+            processor.wrap_with_group({
+            'opacity': str(layer_info[6]),
+        })
+        output_filename2 = os.path.join(temp_dir, 'processed-' + os.path.basename(pctl.GetPlotFileName()))
+        processor.write(output_filename2)
+        processed_svg_files.append((output_filename2, processor))
+
+
+    print 'Merging layers...'
+    final_svg = os.path.join(output_directory, project_name + '-merged.svg')
+
+    shutil.copyfile(processed_svg_files[0][0], final_svg) # use first layer SVG as a container.
+    processed_svg_files.pop(0) # hack so that the first layer is not rendered twice
+    output_processor = SvgProcessor(final_svg)
+    for _, processor in processed_svg_files:
+        output_processor.import_groups(processor) # add new layers to the file.
+    output_processor.write(final_svg)
+
+    print 'Rasterizing...'
+    final_png = os.path.join(output_directory, output_filename)
+
+    subprocess.check_call([
+        'C:\Program Files\Inkscape\inkscape',
+        '--export-area-drawing',
+        '--export-dpi=600',
+        '--export-png', final_png,
+        '--export-background', plot_bg,
+        final_svg,
+    ])
+
+
+
+plot_bg = '#064A00'
+# Once the defaults are set it become pretty easy...
+# I have a Turing-complete programming language here: I'll use it...
+# param 0 is a string added to the file base name to identify the drawing
+# param 1 is the layer ID
+plot_plan = [
+    ( "CuTop", F_Cu, "Top layer", ".gtl", "",'#E8D959',0.85 ),
+    ( "MaskTop", F_Mask, "Mask top", ".gts", "Invert" ,'#1D5D17',0.8 ),
+    ( "PasteTop", F_Paste, "Paste Top", ".gtp", "" ,'#9E9E9E',0.95 ),
+    ( "SilkTop", F_SilkS, "Silk Top", ".gto", "" ,'#fefefe',1.0 ),
+    ( "EdgeCuts", Edge_Cuts, "Edges", ".gml", ""  ,'#000000',0.2 ),
+]
+renderPNG(plot_plan, project_name + '-Front.png')
+
+popt.SetMirror(True)
+plot_plan = [
+    ( "CuBottom", B_Cu, "Bottom layer", ".gbl", "",'#E8D959',0.85 ),
+    ( "MaskBottom", B_Mask, "Mask Bottom", ".gbs", "Invert" ,'#1D5D17',0.8 ),
+    ( "PasteBottom", B_Paste, "Paste Bottom", ".gbp", "" ,'#9E9E9E',0.95 ),
+    ( "SilkTop", B_SilkS, "Silk Bottom", ".gbo", "" ,'#fefefe',1.0 ),
+    ( "EdgeCuts", Edge_Cuts, "Edges", ".gml", ""  ,'#000000',0.2 ),
+]
+renderPNG(plot_plan, project_name + '-Back.png')
+
+
+plot_plan = [
+    ( "CuTop", F_Cu, "Top layer", ".gtl"),
+    ( "CuBottom", B_Cu, "Bottom layer", ".gbl"),
+    ( "MaskBottom", B_Mask, "Mask Bottom", ".gbs"),
+    ( "MaskTop", F_Mask, "Mask top", ".gts"),
+    ( "PasteBottom", B_Paste, "Paste Bottom", ".gbp"),
+    ( "PasteTop", F_Paste, "Paste Top", ".gtp"),
+    ( "SilkTop", F_SilkS, "Silk Top", ".gto"),
+    ( "SilkBottom", B_SilkS, "Silk Bottom", ".gbo"),
+    ( "EdgeCuts", Edge_Cuts, "Edges", ".gml")
+]
+
+popt.SetMirror(False)
+popt.SetDrillMarksType(PCB_PLOT_PARAMS.NO_DRILL_SHAPE)
+print "Plotting Gerber Layers:"
+
+fab_files = []
+
+# Functional Gerber Plots 
+for layer_info in plot_plan:
+    pctl.SetLayer(layer_info[1])
+    pctl.OpenPlotfile(layer_info[0], PLOT_FORMAT_GERBER, layer_info[2])
+    pctl.PlotLayer()
+    time.sleep(0.01)
+    pctl.ClosePlot()
+    # Create a copy with same filename and Protel extensions.
+    srcPlot = pctl.GetPlotFileName()
+    dstPlot = os.path.join(output_directory,project_name + layer_info[3])
+    shutil.move(srcPlot, dstPlot)
+    print layer_info[0] + " => " + dstPlot
+    fab_files.append(dstPlot)
+
+
+#generate internal copper layers, if any
+lyrcnt = board.GetCopperLayerCount();
+
+for innerlyr in range ( 1, lyrcnt-1 ):
+    pctl.SetLayer(innerlyr)
+    lyrname = 'inner%s' % innerlyr
+    pctl.OpenPlotfile(lyrname, PLOT_FORMAT_GERBER, "inner")
+    pctl.PlotLayer()
+    time.sleep(0.01)
+    pctl.ClosePlot()
+    # Create a copy with same filename and Protel extensions.
+    srcPlot = pctl.GetPlotFileName()
+    dstPlot = os.path.join(output_directory,project_name + '.g' + str(innerlyr + 1))
+    shutil.move(srcPlot, dstPlot)
+    print lyrname + " => " + dstPlot
+    fab_files.append(dstPlot)
+
+
+# Fabricators need drill files.
+# sometimes a drill map file is asked (for verification purpose)
+drlwriter = EXCELLON_WRITER( board )
+drlwriter.SetMapFileFormat( PLOT_FORMAT_PDF )
+
+mirror = False
+minimalHeader = False
+#offset = wxPoint(0,0)
+offset = board.GetAuxOrigin()
+mergeNPTH = True
+drlwriter.SetOptions( mirror, minimalHeader, offset, mergeNPTH )
+
+metricFmt = True
+drlwriter.SetFormat( metricFmt )
+
+genDrl = True
+genMap = False
+drlwriter.CreateDrillandMapFilesSet( output_directory, genDrl, genMap );
+
+srcPlot = os.path.join(output_directory,project_name + '.drl')
+dstPlot = os.path.join(output_directory,project_name + '.txt')
+shutil.move(srcPlot, dstPlot)
+print srcPlot + " => " + dstPlot
+fab_files.append(dstPlot)
+
+# One can create a text file to report drill statistics
+rptfn = output_directory + '/drill_report.txt'
+drlwriter.GenDrillReportFile( rptfn );
+
+
+
+#zip up all files
+zf = zipfile.ZipFile(os.path.join(output_directory,project_name + '_' + today + '.zip'), "w", zipfile.ZIP_DEFLATED)
+abs_src = os.path.abspath(output_directory)
+for filename in  fab_files:
+        absname = os.path.abspath(filename)
+        arcname = absname[len(abs_src) + 1:]
+        print 'zipping %s as %s' % (filename,
+                                    arcname)
+        zf.write(absname, arcname)
+zf.close()
+
+shutil.rmtree(temp_dir, ignore_errors=True)
+# We have just generated your plotfiles with a single script
